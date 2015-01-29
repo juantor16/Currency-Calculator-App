9d58c434a96984ba51dd60aeb252103d99fdadf4
juantor16 <juantor16@gmail.com>
2015-01-29 14:57:41 -0300

    Actual android app

---
+++ CurrencyCalculator/app/src/main/java/com/example/juantorres/currencycalculator/calculator.java
@@ -0,0 +1,18 @@
+package com.example.juantorres.currencycalculator;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by Juan Torres on 1/29/2015.
+ */
+public class calculator extends MainActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.firstscreen);
+
+    }
+}
